ffb6abdfc6b4eecf72ee09fe4903dcf4dd384654
manoj <manoj@ojt19-PC.qedge.com>
2020-01-22 18:18:47 +0530

    java

--- src/test/java/testRunner/Runner.java
+++ src/test/java/testRunner/Runner.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="FeaturesFile",glue="stepDefinitions",monochrome=true,plugin={"com.cucumber.listener.ExtentCucumberFormatter:Reports/report.html","pretty","html:target/cucumber-reports"})
+@CucumberOptions(features="FeaturesFile",glue="stepDefinitions",plugin={"com.cucumber.listener.ExtentCucumberFormatter:Reports/report.html","pretty","html:target/cucumber-reports"})
 public class Runner {
 
 }
